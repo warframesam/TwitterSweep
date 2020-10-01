c7f5513969af3660705f22f9bd8662132e05d2bb
warframesam <warframesam@gmail.com>
2020-10-01 12:49:02 +0530

    Basic Fixes

--- src/main/java/com/example/sweep/TweetController.java
+++ src/main/java/com/example/sweep/TweetController.java
@@ -5,7 +5,6 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
-
 @Controller
 public class TweetController {
 	
